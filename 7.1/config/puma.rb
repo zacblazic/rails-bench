cc671943d91ebc50b06eb6744e644a297c4817fd
Zac Blazic <zacblazic@gmail.com>
2024-04-21 14:52:43 +0200

    Add puma config file

--- 7.1/config/puma.rb
+++ 7.1/config/puma.rb
@@ -1,35 +1,41 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+# Whether to run in single mode or cluster mode.
+mode = ENV['PUMA_MODE'] || 'single'
 
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+# Server address bind configuration.
+bind_address = ENV['PUMA_BIND_ADDRESS'] || '127.0.0.1'
+bind_port = ENV['PUMA_BIND_PORT'] || 3000
+bind = "tcp://#{bind_address}:#{bind_port}"
 
-# Specifies that the worker count should equal the number of processors in production.
-if ENV["RAILS_ENV"] == "production"
-  require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
-  workers worker_count if worker_count > 1
+# Configure defaults based on mode.
+if mode == 'single'
+  workers = 0
+  threads_min = ENV['PUMA_THREADS_MIN']&.to_i || 1
+  threads_max = ENV['PUMA_THREADS_MAX']&.to_i || 1
+  preload_app = ENV['PUMA_PRELOAD_APP'].nil? ? false : ENV['PUMA_PRELOAD_APP'] == 'true'
+else
+  workers = ENV['PUMA_WORKERS']&.to_i || 3
+  threads_min = ENV['PUMA_THREADS_MIN']&.to_i || 2
+  threads_max = ENV['PUMA_THREADS_MAX']&.to_i || 2
+  preload_app = ENV['PUMA_PRELOAD_APP'].nil? ? true : ENV['PUMA_PRELOAD_APP'] == 'true'
 end
 
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+# Disable queue requests if running with a single thread.
+queue_requests = ENV['PUMA_DISABLE_QUEUE_REQUESTS'] != 'true' && threads_max > 1
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# Perform configuration via Puma DSL.
+bind bind
+workers workers
+threads threads_min, threads_max
+preload_app! if preload_app
+queue_requests queue_requests
 
-# Specifies the `environment` that Puma will run in.
-environment ENV.fetch("RAILS_ENV") { "development" }
-
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
-# Allow puma to be restarted by `bin/rails restart` command.
-plugin :tmp_restart
+on_booted do
+  # Log configuration.
+  Rails.logger.info "[puma] Mode: #{mode}"
+  Rails.logger.info "[puma] Bind: #{bind}"
+  Rails.logger.info "[puma] Workers: #{workers}"
+  Rails.logger.info "[puma] Threads min: #{threads_min}"
+  Rails.logger.info "[puma] Threads max: #{threads_max}"
+  Rails.logger.info "[puma] Preload: #{preload_app}"
+  Rails.logger.info "[puma] Queue requests: #{queue_requests}"
+end
